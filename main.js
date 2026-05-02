0a119333572f0253afe1ff45c3ea6cfeaba1eaed
Evgeniy-makdak <makdak23@mail.ru>
2026-05-02 20:27:00 +0300

    Доработал логику

--- main.js
+++ main.js
@@ -3,18 +3,23 @@ const hours = document.querySelector(".timer_hours"),
   seconds = document.querySelector(".timer_seconds");
 
 const t = setInterval(function countDown() {
-  if (+hours.textContent == 0 && +minuts.textContent == 0 && +seconds.textContent == 0) {
+  if (+seconds.textContent > 0) {
+    seconds.textContent = +seconds.textContent - 1;
+  } else if (+minuts.textContent > 0) {
+    minuts.textContent = +minuts.textContent - 1;
+    seconds.textContent = 59;
+  } else if (+hours.textContent > 0) {
+    hours.textContent = +hours.textContent - 1;
+    minuts.textContent = 59;
+    seconds.textContent = 59;
+  }
+
+  if (
+    +hours.textContent == 0 &&
+    +minuts.textContent == 0 &&
+    +seconds.textContent == 0
+  ) {
     clearInterval(t);
-  } else {
-    if (+seconds.textContent > 0) {
-      seconds.textContent = +seconds.textContent - 1;
-    } else if (+minuts.textContent > 0) {
-      minuts.textContent = +minuts.textContent - 1;
-      seconds.textContent = 59;
-    } else {
-      hours.textContent = +hours.textContent - 1;
-      minuts.textContent = 59;
-    }
   }
 }, 1000);
 
